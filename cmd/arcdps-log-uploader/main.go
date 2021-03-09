733425a79b695dba536be0cfcf122aea3aa68a1a
Tobias Günther <Xyaren@users.noreply.github.com>
2021-03-10 00:10:17 +0100

    fix fixes #2 Display a warning when running in administrator mode. Drag & Drop from user into processes with elevated privileges is not possible in windows.

--- cmd/arcdps-log-uploader/main.go
+++ cmd/arcdps-log-uploader/main.go
@@ -3,16 +3,34 @@
 package main
 
 import (
+	"github.com/lxn/walk"
 	log "github.com/sirupsen/logrus"
+	"os"
 )
 
 func main() {
 	setupLogging()
 	log.Info("Starting")
 
-	err := startUi()
+	if runningWithAdminPrivileges() {
+		walk.MsgBox(nil, "Administrator mode is not supported", "Due to windows security constraints, drag & drop is not supported for applications running in administrative mode.", walk.MsgBoxOK|walk.MsgBoxIconWarning|walk.MsgBoxTaskModal)
+	} else {
+		start()
+	}
+	log.Info("Bye")
+}
+
+func start() {
+	var err = startUi()
 	if err != nil {
 		panic(err)
 	}
-	log.Info("Bye")
+}
+
+func runningWithAdminPrivileges() bool {
+	_, err := os.Open("\\\\.\\PHYSICALDRIVE0")
+	if err != nil {
+		return false
+	}
+	return true
 }
